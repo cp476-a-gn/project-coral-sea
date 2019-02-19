74daa7bf97fe395b4529c4f2ad522e5e5c62c84a
Nicholas Hallman <ngahallman@gmail.com>
2019-02-19 14:08:49 -0500

    css files and score added

--- routes/roots.js
+++ routes/roots.js
@@ -4,4 +4,7 @@ module.exports = function(app){
     app.get('/',function(req,res){
         res.render('pages/MainMenu.ejs');
     });
+    app.get('/score', function(req,res){
+        res.render("pages/ScoreBoard.ejs");
+    });
 }
