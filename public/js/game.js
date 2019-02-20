6814a0affe60e7f12b758a14626f568218efaf1f
Nicholas Hallman <ngahallman@gmail.com>
2019-02-19 21:08:17 -0500

    Added the game page

---
+++ public/js/game.js
@@ -0,0 +1,13 @@
+
+let type="WebGL";
+if(!PIXI.utils.isWebGLSupported()){
+    type="canvas";
+}
+PIXI.utils.sayHello(type);
+
+let app = new PIXI.Application({width: 256, height: 256});
+
+document.body.appendChild(app.view);
+
+app.renderer.ClassList.add();
+console.log("???");
--- routes/roots.js
+++ routes/roots.js
@@ -16,9 +16,12 @@ module.exports = function(app){
 		app.get('/mainMenu',function(req,res){
         res.render('pages/MainMenu.ejs');
     });
- 
+
     app.get('/goToScore',function(req,res){
 	    console.log(req.body.return);
         res.render('pages/ScoreBoard.ejs');
+    });
+    app.get('/game',function(req,res){
+        res.render("pages/Game.ejs");
     });
 }
