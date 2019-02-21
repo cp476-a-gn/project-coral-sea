facbcc14e9257aeb1421b198a53097a01b15cc68
Nicholas Hallman <ngahallman@gmail.com>
2019-02-21 16:31:38 -0500

    added main spice

---
+++ public/js/mainAnimate.js
@@ -0,0 +1,18 @@
+smalWave = null;
+mediWave = null;
+largWave = null
+
+
+window.onload = function(){
+
+    smalWave = document.getElementById("wave1");
+    mediWave = document.getElementById("wave2");
+    largWave = document.getElementById("wave3");
+
+    setInterval(moveWaves, 16);
+
+}
+
+function moveWaves(){
+
+}
