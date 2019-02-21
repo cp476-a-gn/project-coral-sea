7de08a62399ca9cc10cf7b9a711a7d645b83d705
Nicholas Hallman <ngahallman@gmail.com>
2019-02-21 18:02:13 -0500

    re styled main

--- public/js/mainMenuBar.js
+++ public/js/mainMenuBar.js
@@ -1,11 +1,11 @@
 function openRegister(){
-	document.getElementById("sidebar").style.width = "30vw";
-	document.getElementById("pas_rep").style.display = "block"; 
+	document.getElementById("sidebar").style.width = "25vw";
+	document.getElementById("pas_rep").style.display = "flex"; 
 	document.getElementById("reg_name").innerHTML = "Registration"; 
 }
 
 function openLogin(){
-	document.getElementById("sidebar").style.width = "30vw";
+	document.getElementById("sidebar").style.width = "25vw";
 	document.getElementById("pas_rep").style.display = "none"; 
 	document.getElementById("reg_name").innerHTML = "Login"; 
 }
