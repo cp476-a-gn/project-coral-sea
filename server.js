f09dd8ac2970b0ade88cce539da02d1ab4f074ee
Nicholas Hallman <ngahallman@gmail.com>
2019-03-16 16:40:29 -0400

    Queue created
    when two clients join the queue the game redirects them to the main application

--- server.js
+++ server.js
@@ -28,17 +28,39 @@ require('./routes/roots.js')(app);
 http.listen(port, '0.0.0.0', function(){
     console.log('Server Started on Port: ' + port);
 });
-
-
+current_room = 0
+first = true;
 io.on('connection', function(socket){
 
-    console.log("A user has sailed!");
+	socket.on('add to queue', function(msg){
+		room = 0;
+
+		if(first){
+			room = current_room;
+			first = !first;
+			socket.join(room);
+			console.log("User has joined room " + room);
+		}
+		else if(!first){
+			room = current_room;
+			current_room ++;
+			first = !first;
+			socket.join(room);
+			io.sockets.in(room).emit("start game");
+			console.log("User has joined room " + room);
+		}
+	});
+	
+
     socket.on('disconnect', function(socket){
         console.log("A user has sailed to deeper waters!");
     });
     socket.on('player ready', function(msg){
         console.log("player ships submited");
-    });
+	});
+	socket.on('join queue', function(msg){
+		console.log("player joined queue" + msg);
+	})
 
 		socket.on('getBoard', function(){
 				function listUserNames(userIDs) {
