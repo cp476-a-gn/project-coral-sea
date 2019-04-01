2aa0e1689e63074a00f13920eb2539491751ed09
vash-evg <vashaevg@gmail.com>
2019-03-31 21:04:11 -0400

    turns

--- public/js/game.js
+++ public/js/game.js
@@ -62,7 +62,7 @@ loader.onComplete.add(() => {
         if(j % val == 0 && j != 0){ starth = starth + (unit_size * 1.5); startx = beginx; val+=1; j = 0}
         sprites.ships[i].id = i;
         sprites.ships[i].width = (sprites.ships[i].width / sprites.ships[i].height) * unit_size;
-		sprites.ships[i].height = unit_size;
+				sprites.ships[i].height = unit_size;
 
         
         sprites.ships[i].x = startx;
