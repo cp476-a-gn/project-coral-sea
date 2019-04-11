4724fc5bc364d2541cd29bb49f97228ad8e20ac7
Nicholas Hallman <ngahallman@gmail.com>
2019-04-10 23:08:15 -0400

    removed type debug

--- Game Server/gameServer.js
+++ Game Server/gameServer.js
@@ -14,7 +14,7 @@ module.exports = function(app, io){
         
         socket.on('add to queue', function(msg){
             room = 0;
-            console.log(typeof socket.handshake.session.room);
+            console.log(socket.handshake.session.room);
             if(first && typeof socket.handshake.session.room == null){
                 room = current_room;
                 first = !first;
